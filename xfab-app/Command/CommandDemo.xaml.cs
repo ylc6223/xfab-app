b68df89e8ee3b54a398ca85c6d7cabc63565154d
Jarry Yang <260902108@qq.com>
2022-09-28 18:16:49 +0800

    change on 0928/2022

---
+++ xfab-app/Command/CommandDemo.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace xfab_app.Command
+{
+    public partial class CommandDemo : Window
+    {
+        public CommandDemo()
+        {
+            InitializeComponent();
+            this.DataContext = new MainViewModel();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("点击了按钮");
+        }
+    }
+}
---
+++ xfab-app/Command/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using  System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace xfab_app.Command
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            ShowCommand = new MyCommand(Show);
+        }
+        public MyCommand ShowCommand { get; set; }
+        public void Show()
+        {
+            MessageBox.Show("点击了按钮");
+        }
+    }
+}
