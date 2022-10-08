12b4076bd6130526c86817faedd90bb9e15e1abb
Jarry Yang <260902108@qq.com>
2022-10-08 12:55:10 +0800

    1008

---
+++ xfab-app/ExpenseItHome.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace xfab_app
+{
+    public partial class ExpenseItHome : Page
+    {
+        public ExpenseItHome()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ xfab-app/MainWindow1.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace xfab_app
+{
+    public partial class MainWindow1 : NavigationWindow
+    {
+        public MainWindow1()
+        {
+            InitializeComponent();
+        }
+    }
+}
