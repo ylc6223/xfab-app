cb02e91695e0ba8088e43af38f4a0aa52beda64b
Jarry Yang <260902108@qq.com>
2022-10-09 18:58:21 +0800

    1010 18:58

---
+++ xfab-app/ContentContrls/TextBoxDemo.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows;
+
+namespace xfab_app.ContentContrls
+{
+    public partial class TextBoxDemo : Window
+    {
+        public TextBoxDemo()
+        {
+            InitializeComponent();
+            tbSettingText.Text = "Initial text contents of the TextBox.";
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            // this.tbSettingText.Text = "hello world";
+            string selectedText = this.tbSettingText.SelectedText;
+            int selectedIndex = this.tbSettingText.SelectionStart;
+            Console.WriteLine(selectedText);
+            Console.WriteLine(selectedIndex);
+        }
+    }
+}
